a2b98fe4c0dcfc999a222302a02c424bad468799
Raphael Riebl <raphael.riebl@thi.de>
2015-01-05 19:58:31 +0100

    fix TraCIScenarioManagerInet

--- src/veins/modules/mobility/traci/TraCIScenarioManagerInet.cc
+++ src/veins/modules/mobility/traci/TraCIScenarioManagerInet.cc
@@ -3,17 +3,17 @@
 #ifdef WITH_INET
 
 #define WITH_TRACI
-#include "mobility/single/TraCIMobility.h"
+#include "inet/mobility/group/TraCIMobility.h"
 
 void ifInetTraCIMobilityCallPreInitialize(cModule* mod, const std::string& nodeId, const Coord& position, const std::string& road_id, double speed, double angle) {
-	::TraCIMobility* inetmm = dynamic_cast< ::TraCIMobility*>(mod);
+	inet::TraCIMobility* inetmm = dynamic_cast<inet::TraCIMobility*>(mod);
 	if (!inetmm) return;
 	inetmm->preInitialize(nodeId, ::Coord(position.x, position.y), road_id, speed, angle);
 }
 
 
 void ifInetTraCIMobilityCallNextPosition(cModule* mod, const Coord& p, const std::string& edge, double speed, double angle) {
-	::TraCIMobility *inetmm = dynamic_cast< ::TraCIMobility*>(mod);
+	inet::TraCIMobility *inetmm = dynamic_cast<inet::TraCIMobility*>(mod);
 	if (!inetmm) return;
 	inetmm->nextPosition(::Coord(p.x, p.y), edge, speed, angle);
 }
